2a279537462e79446ea7ae1f0b27dcb72f5a36f8
Afraz Khan <afrazarif@hotmail.com>
2024-07-18 15:10:03 -0700

    Added in some work on the High res req

--- workflows/part2_version2/create_error_tables.sh
+++ workflows/part2_version2/create_error_tables.sh
@@ -1,15 +1,15 @@
 # Define variables for file paths
 SCRIPT_PATH="/arc/project/st-ashapi01-1/git/afraz96/RADD/workflows/part2_version2/generate_model_stats.py"
-OUTPUT_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/NPS_OUTPUT"
+OUTPUT_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/X500R_output"
 DATA_PATH="/arc/project/st-ashapi01-1/git/afraz96/RADD/workflows/part2_version2/Data/training_data_bccdc_20240708.csv"
 TEST_DATA_PATH="/arc/project/st-ashapi01-1/git/afraz96/RADD/workflows/part2_version2/Data/X500R_SMILES.csv"
-MODELS_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/NPS_OUTPUT/models"
-PROCESSOR_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/NPS_OUTPUT/processor.pkl"
+MODELS_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/X500R_output/models"
+PROCESSOR_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/X500R_output/processor.pkl"
 # Print the date and time
 echo "Job started at: $(date)"
 
 # Run the Python script with specified arguments
-python $SCRIPT_PATH -o $OUTPUT_PATH -d $DATA_PATH -t $TEST_DATA_PATH -m $MODELS_PATH -p $PROCESSOR_PATH
+python $SCRIPT_PATH -o $OUTPUT_PATH -d $TEST_DATA_PATH -t $DATA_PATH -m $MODELS_PATH -p $PROCESSOR_PATH
 
 # Print the date and time
 echo "Job finished at: $(date)"
