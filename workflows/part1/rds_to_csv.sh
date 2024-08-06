16837ba768a4bbb9868932b6b8e8c94006461c27
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-08-05 20:57:00 -0700

    Update rds_to_csv.sh
    add comments

--- workflows/part1/rds_to_csv.sh
+++ workflows/part1/rds_to_csv.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script is meant to run interactively, **not** as a SLURM job submission
+#
+# cd /scratch/
+# bash rds_to_csv.sh rds_location_absolute_path 
+
 module load gcc/5.5.0 r/4.4.0
 
 Rscript rds_to_csv.R $1 > $1.log 
