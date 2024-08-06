14694271eaf6d5c1a604bce48f2dba0a57f1a453
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-08-05 20:37:47 -0700

    Update go_loop.sh
    updated path to go_loop.R

--- workflows/part1/go_loop.sh
+++ workflows/part1/go_loop.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#SBATCH --time=30:00:00                    # Request 3 hours of runtime
-#SBATCH --account=st-ashapi01-1            # Specify your allocation code
+#SBATCH --time=30:00:00                 # Request 3 hours of runtime
+#SBATCH --account=st-ashapi01-1         # Specify your allocation code
 #SBATCH --job-name=radd-p1              # Specify the job name
 #SBATCH --nodes=1                       # Defines the number of nodes for each sub-job.
 #SBATCH --ntasks-per-node=1             # Defines tasks per node for each sub-job.
-#SBATCH --mem=32G                        # Request 8 GB of memory
-#SBATCH --array=1-5 
+#SBATCH --mem=32G                       # Request 8 GB of memory
+#SBATCH --array=1-50 
 #SBATCH --error=%A_%a.err
-##  SBATCH --mail-user=lisa.tang@bccdc.ca   # Email address for job notifications
-##  SBATCH --mail-type=ALL                 # Receive email notifications for all job events
 
 export LC_ALL=C; unset LANGUAGE
 
@@ -18,28 +16,17 @@ output_dir=$3
 module load gcc
 module load apptainer
 
-# Create output folder if not already created; now done in R
-# if [ ! -d ${output_dir} ]; then
-#  mkdir -p $output_dir;
-# fi
-
-
+script_path='/arc/project/st-ashapi01-1/git/ywtang/RADD/workflows/part1/go_loop.R'
 
 # Compile the command 
-cmd="/arc/project/st-ashapi01-1/RADD/library_final/apptainer/ubuntu.sandbox Rscript /arc/project/st-ashapi01-1/RADD/workflow1/"
-cmd="${cmd}go_loop.R ${1} ${2} ${output_dir} ${SLURM_ARRAY_TASK_ID} ${4} ${5}"
+cmd="/arc/project/st-ashapi01-1/RADD/library_final/apptainer/ubuntu.sandbox Rscript ${script_path}"
+cmd="${cmd} ${1} ${2} ${output_dir} ${SLURM_ARRAY_TASK_ID} ${4} ${5}"
 echo -e "Command: $cmd"
 
-
 # Call with the cmd which executes inner.R script that will loop over files found by system call in R
-
 if [ "$#" -eq 5 ]; then
   apptainer exec --nv $cmd
 else
   echo -e "Error in the sbatch argument list: [mzML_folder] [database_csv] [output_path] [number_of_batches] [VERBOSE] \nAll files will be divided into N job arrays N=[number_of_batches]."
   set -e
 fi
-
-
-
-
