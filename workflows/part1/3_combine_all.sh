9f151484138b0269cf3002239fe03a06a89dc10f
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-08-05 20:54:41 -0700

    Update 3_combine_all.sh

--- workflows/part1/3_combine_all.sh
+++ workflows/part1/3_combine_all.sh
@@ -16,19 +16,19 @@ file=2022-0140BG01
 head -n 1 ${file}_ms1.csv > combined_ms1.txt && tail -n+2 -q *ms1.csv >> combined_ms1.txt
 head -n 1 ${file}_ms2.csv > combined_ms2.txt && tail -n+2 -q *ms2.csv >> combined_ms2.txt
 
-
-
-
-cd /scratch/st-ashapi01-1/expedited_2023/combined_db_20240801/
-file=2023-0001BG01
-head -n 1 ${file}_ms1.csv > combined_ms1.txt && tail -n+2 -q *ms1.csv >> combined_ms1.txt
-head -n 1 ${file}_ms2.csv > combined_ms2.txt && tail -n+2 -q *ms2.csv >> combined_ms2.txt
-
 cd /scratch/st-ashapi01-1/expedited_2021/combined_db_20240801
 file=2021-0301BG01   
 head -n 1 ${file}_ms1.csv > combined_ms1.txt && tail -n+2 -q *ms1.csv >> combined_ms1.txt
 head -n 1 ${file}_ms2.csv > combined_ms2.txt && tail -n+2 -q *ms2.csv >> combined_ms2.txt
 
+
+
+# 2024-08-05 - not completed yet; need to wait few hours and then copy-paste below
+
+cd /scratch/st-ashapi01-1/expedited_2023/combined_db_20240801/
+file=2023-0001BG01
+head -n 1 ${file}_ms1.csv > combined_ms1.txt && tail -n+2 -q *ms1.csv >> combined_ms1.txt
+head -n 1 ${file}_ms2.csv > combined_ms2.txt && tail -n+2 -q *ms2.csv >> combined_ms2.txt
 
 
 
