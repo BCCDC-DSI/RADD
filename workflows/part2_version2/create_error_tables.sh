e1ca8cc180e4ba9f2db89b56c146a3071767119b
Afraz Khan <afrazarif@hotmail.com>
2024-10-07 16:57:45 -0700

    fixed the repo structure, unified requirements

--- workflows/part2_version2/create_error_tables.sh
+++ workflows/part2_version2/create_error_tables.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=ml_pipeline         # Job name
+#SBATCH --account=st-ashapi01-1        # Specify your allocation code
+#SBATCH --job-name=smiles_ml_pipeline  # Specify the job name
+#SBATCH --ntasks=8                     # Number of tasks (processes)
+#SBATCH --nodes=1                      # Number of nodes
+#SBATCH --time=140:00:00               # Time limit hrs:min:sec
+#SBATCH --mem=8G                       # Memory limit
+#SBATCH --array=1-5                    # Job array
+#SBATCH --output=logs/array_%A_%a.out  # Redirects standard output to unique files for each sub-job.
+#SBATCH --error=logs/array_%A_%a.err   # Redirects standard error to unique files for each sub-job.
+#SBATCH --partition=skylake            # Partition name
 # Define variables for file paths
 SCRIPT_PATH="/arc/project/st-ashapi01-1/git/afraz96/RADD/workflows/part2_version2/generate_model_stats.py"
 OUTPUT_PATH="/scratch/st-ashapi01-1/RADD/SMILES_ML_PIPELINE/X500R_output"
